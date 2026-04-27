3e2c91ed8f68cf13705a3263ac88cc3c9e4176e1
Sabeel Khan <sabeel.khan@Sabeel-ka-MacBook-Pro.local>
2026-04-27 09:31:26 +0530

    HOF

--- Javascript/Class-2/HOF/script.js
+++ Javascript/Class-2/HOF/script.js
@@ -47,3 +47,58 @@ download(newDummy);
 */
 
 const radii = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+// function perimeters(radii) {
+//     const res = [];
+//     for (let radius of radii) {
+//         res.push(2 * Math.PI * radius);
+//     }
+//     return res;
+// }
+
+// function areas(radii) {
+//     const res = [];
+//     for (let radius of radii) {
+//         res.push( Math.PI * radius * radius);
+//     }
+//     return res;
+// }
+
+// function diameters(radii) {
+//     const res = [];
+//     for (let radius of radii) {
+//         res.push( 2* radius);
+//     }
+//     return res;
+// }
+
+// console.log(perimeters(radii));
+// console.log(areas(radii));
+// console.log(diameters(radii));
+
+function perimeter(radius) {
+    return 2 * Math.PI * radius;
+}
+
+function area(radius) {
+    return Math.PI * radius * radius;
+}
+
+function diameter(radius) {
+    return 2 * radius;
+}
+
+function calculate(radii, callback) {
+    const res = [];
+    for (let radius of radii) {
+        res.push(callback(radius));
+    }
+    return res;
+}
+
+console.log(calculate(radii, perimeter));
+console.log(calculate(radii, area));
+console.log(calculate(radii, diameter));
+
+
